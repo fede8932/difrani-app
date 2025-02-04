227f49e9c44585fe5ec406707ffdeee15589fca8
fede8932 <federi.angel@gmail.com>
2025-02-04 14:57:54 -0300

    act

--- src/commonds/newMenu/NewMenu.jsx
+++ src/commonds/newMenu/NewMenu.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 import {
   DropdownMenu,
   DropdownItem,
   DropdownDivider,
   Dropdown,
-} from 'semantic-ui-react';
-import CustomMenu from '../menu/CustomMenu';
-import ProtectedComponent from '../../protected/protectedComponent/ProtectedComponent';
+} from "semantic-ui-react";
+import CustomMenu from "../menu/CustomMenu";
+import ProtectedComponent from "../../protected/protectedComponent/ProtectedComponent";
 const redirect_url = import.meta.env.VITE_REDIRECT_URL;
 const entorno = import.meta.env.VITE_ENTORNO;
 
 function NewMenu(props) {
   const { logOutFn, perfilFn, ...rest } = props;
 
-  const redirectToGoogle = () => {
-    window.location.href = redirect_url;
+  const redirectToGoogle = (url) => {
+    window.location.href = url ?? redirect_url;
   };
   return (
     <Dropdown text="File" trigger={<CustomMenu />} icon={null} {...rest}>
@@ -26,16 +26,32 @@ function NewMenu(props) {
           }}
         />
         <DropdownDivider />
-        <ProtectedComponent listAccesss={[1]}>
-          <DropdownItem
-            text={
-              entorno == 'BLASE'
-                ? 'Ir a Difrani'
-                : 'Ir a Blase'
-            }
-            onClick={redirectToGoogle}
-          />
-        </ProtectedComponent>
+        {entorno !== "BLASE" ? (
+          <ProtectedComponent listAccesss={[1]}>
+            <DropdownItem
+              text="Ir a Blase"
+              onClick={() =>
+                redirectToGoogle("https://admin.blasedistribuidora.com")
+              }
+            />
+          </ProtectedComponent>
+        ) : null}
+        {entorno !== "DIFRANI" ? (
+          <ProtectedComponent listAccesss={[1]}>
+            <DropdownItem
+              text="Ir a Difrani"
+              onClick={() => redirectToGoogle("https://system.difrani.com")}
+            />
+          </ProtectedComponent>
+        ) : null}
+        {entorno !== "ALOSPITS" ? (
+          <ProtectedComponent listAccesss={[1]}>
+            <DropdownItem
+              text="Ir a Alospits"
+              onClick={() => redirectToGoogle("https://admin.alospits.com")}
+            />
+          </ProtectedComponent>
+        ) : null}
         <DropdownItem text="Cerrar sesión" onClick={() => logOutFn()} />
       </DropdownMenu>
     </Dropdown>
